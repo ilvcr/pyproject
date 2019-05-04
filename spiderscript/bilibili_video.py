b2fb25addb1bcb0ac76b25f7f5f06fbc1a3ad509
Miao <leeyog@163.com>
2019-05-04 17:56:22 +0800

    g

---
+++ spiderscript/bilibili_video.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#*************************************************************************#
+# File Name: bilibili_video.py
+# Author: yoghourt->ilvcr 
+# Mail: liyaoliu@foxmail.com  @@  ilvcr@outlook.com 
+# Created Time: Sat May  4 17:56:12 2019
+# Description: 
+#************************************************************************#
+
+
---
+++ spiderscript/douban_spider.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#*************************************************************************#
+# File Name: douban_spider.py
+# Author: yoghourt->ilvcr 
+# Mail: liyaoliu@foxmail.com  @@  ilvcr@outlook.com 
+# Created Time: Sat May  4 17:53:06 2019
+# Description: 
+#************************************************************************#
+
+
